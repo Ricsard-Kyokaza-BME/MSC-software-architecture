21965a8353cf67bafd24b394123cd4e865af1bcc
Richard Balog <richard@oktafone.com>
2016-11-12 14:05:24 +0100

    Angular material updated, index page started

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,13 +1,28 @@
 var app = angular.module('HotelReservation', ['ui.router', 'ngFileUpload', 'ngMaterial']);
-app.config(function($stateProvider) {
+app.config(function($stateProvider, $mdIconProvider) {
+    $mdIconProvider.fontSet('md', 'material-icons');
+
+
     var indexState = {
         name: 'index',
         url: '/',
-        template: '<h3>hello world!</h3>'
+        templateUrl: '/views/index.html'
+    };
+
+    var helloState = {
+        name: 'hello',
+        url: '/hello',
+        template: '<h3>Hello</h3>'
     };
 
     $stateProvider.state(indexState);
+    $stateProvider.state(helloState);
 });
-app.controller('BootCtrl', function($scope, $state){
+app.controller('BootCtrl', function($scope, $state, $rootScope){
     $state.go('index');
+
+    $rootScope.$on('$stateChangeSuccess',
+        function(event, toState, toParams, fromState, fromParams){
+            $scope.currentNavItem = toState.name;
+        });
 });
