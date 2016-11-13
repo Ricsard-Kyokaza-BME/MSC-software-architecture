56ea933f613c15cb9c16e2c79ac11f089a263a74
Richard Balog <richard@oktafone.com>
2016-11-13 15:29:09 +0100

    Added CRUDL routes for reviews

--- models/review.js
+++ models/review.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Review',{
     owner:       String,
     title:       String,
+    hotelId:     String,
     description: String,
     date:        Date,
     rating:      Number
