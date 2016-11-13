cb9ceb6ab551855ca53b0598ff1330c2403b1f92
emakazi <Marton.Kazinczi@ericsson.com>
2016-11-13 23:55:33 +0100

    Vanbaj

--- public/features/hotelAdministration/hotelCreate.js
+++ public/features/hotelAdministration/hotelCreate.js
@@ -9,6 +9,7 @@ function userLoginCtrlConstructor($scope, $injector){
     var StateHandler = $injector.get('StateHandler');
     var Hotel = $injector.get('Hotel');
     var Room = $injector.get('Room');
+    var Upload = $injector.get('Upload');
 
 
     var vm = this;
@@ -18,10 +19,14 @@ function userLoginCtrlConstructor($scope, $injector){
     vm.images = [];
 
     vm.createHotel = function () {
-        $http.post('/hotel', hotel)
+        vm.hotel.rooms = _.map(vm.roomsToAdd, function (element) {
+           return element.transformToSend();
+        });
+        $http.post('/hotel', vm.hotel)
             .success(function(data) {
                 console.log(data);
                 $state.go(StateHandler.getPreviousStateName());
+                vm.uploadImages(data._id);
             })
             .error(function(err) {
                 console.log(err);
@@ -40,13 +45,23 @@ function userLoginCtrlConstructor($scope, $injector){
         if(vm.room.type!='' && vm.room.description!='' && vm.room.capacity!=0 && vm.room.quantity!=0){
             vm.roomsToAdd.push(angular.copy(vm.room));
             vm.room = new Room();
-        }else{
-
         }
     };
 
-    vm.uploadImages = function () {
-      console.log(vm.images);
+    vm.uploadImages = function (hotelID) {
+        Upload.upload({
+            url: '/hotel/' + hotelID + '/image',
+            method: 'POST',
+            file: vm.images
+        }).then(function (resp) {
+            console.log('Images upload successful');
+            console.log(resp);
+        }, function (resp) {
+            console.log('Error status: ' + resp.status);
+        }, function (evt) {
+            // $scope.uploadProgress = parseInt(100.0 * evt.loaded / evt.total);
+            // console.log('progress: ' + $scope.uploadProgress + '% ');
+        });
     };
 
     vm.deleteImage = function (image) {
