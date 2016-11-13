d4e1f472a31c47a6b8ae40cc12157fcb33ccc9a3
Richard Balog <richard@oktafone.com>
2016-11-13 18:54:45 +0100

    Hotel description field added, SessionService refactored

--- models/hotel.js
+++ models/hotel.js
@@ -3,6 +3,7 @@ var mongoose = require('mongoose');
 module.exports = mongoose.model('Hotel',{
     name:          String,
     owner:         String,
+    description:   String,
     location:      String,
     luxuryLevel:   Number,
     reviews:       [String],
