0f8eba65ca9ebe93a1d976d54d23d8ae484dcf6b
Richard Balog <richard@oktafone.com>
2016-11-13 18:44:53 +0100

    Unauthorized middleware updated to give back 401

--- commonFunctions.js
+++ commonFunctions.js
@@ -15,10 +15,14 @@ var isAuthenticated = function (req, res, next) {
     // if user is authenticated in the session, call the next() to call the next request handler
     // Passport adds this method to request object. A middleware is allowed to add properties to
     // request and response objects
-    if (req.isAuthenticated())
+    if (req.isAuthenticated()) {
         return next();
+    }
     // if the user is not authenticated then redirect him to the login page
-    res.redirect('/');
+    else {
+        res.status(401);
+        res.json({error: "Unauthorized for this operation!"})
+    }
 };
 
 var hasGuestLevel = function(req, res, next) {
