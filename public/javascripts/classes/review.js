d6ad1115b4916e8b0b5dabac995c92f31c1f36d5
Richard Balog <richard@oktafone.com>
2016-11-16 20:52:41 +0100

    Review title removed, roomType enum created on FE-BE

--- public/javascripts/classes/review.js
+++ public/javascripts/classes/review.js
@@ -4,17 +4,15 @@ app.factory( "Review", function() {
      * Constructor
      * @param {string} id
      * @param {string} owner
-     * @param {string} title
      * @param {string} hotelId
      * @param {string} description
      * @param {Date} date
      * @param {number} rating
      * @constructor
      */
-    function Review( id, owner, title, hotelId, description, date, rating) {
+    function Review( id, owner, hotelId, description, date, rating) {
         this.id = id || '';
         this.owner = owner || '';
-        this.title = title || '';
         this.hotelId = hotelId || '';
         this.description = description || '';
         this.date = date || new Date();
