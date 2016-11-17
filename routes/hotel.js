ab9299d66f6982d4d7ce87f44a91c93b9ba076d3
Richard Balog <richard@oktafone.com>
2016-11-17 17:32:09 +0100

    Route added to create room. Routes belongs to room moved to separated file

--- routes/hotel.js
+++ routes/hotel.js
@@ -128,23 +128,6 @@ router.post('/:id/rooms', function(req, res, next) {
         });
 });
 
-/* DELETE specified room of a specified hotel. */
-router.delete('/:hotelId/room/:roomId', function(req, res, next) {
-    Room
-        .findOneAndRemove({_id: req.params.roomId})
-        .exec(function(err, room){
-            if (err){
-                commons.sendError(req, res, 'Error in getting hotel\'s rooms', err);
-            } else {
-                Reservation.remove({ _id: { $in: rooms.reservations } }, function (err) {
-                    err ? commons.sendError(req, res, 'Error in removing rooms', err) : '';
-                });
-
-                res.json({id: room._id});
-            }
-        });
-});
-
 /* GET hotel image. */
 router.get('/:id/image/:imageId', function(req, res, next) {
     res.sendFile(path.join(__dirname, '../upload', req.params.imageId));
---
+++ routes/room.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+
+var Hotel = require('../models/hotel');
+var Room = require('../models/room');
+var Reservation = require('../models/reservation');
+var commons = require('../commonFunctions');
+
+/* POST add a room to a specified hotel. */
+router.post('/:hotelId/room', function(req, res, next) {
+    Room.create(req.body, function (err, room) {
+        if (err){
+            commons.sendError(req, res, 'Error in creating room', err);
+        } else {
+            Hotel.findOneAndUpdate({ _id: req.params.hotelId }, {rooms: {$push: room._id}}, function (err, hotel) {
+                err ? commons.sendError(req, res, 'Error in adding room to the hotel', err) : '';
+            });
+
+            res.json(room);
+        }
+    });
+});
+
+/* DELETE specified room of a specified hotel. */
+router.delete('/:hotelId/room/:roomId', function(req, res, next) {
+    Room.findOneAndRemove({_id: req.params.roomId}, function(err, room){
+            if (err){
+                commons.sendError(req, res, 'Error in getting hotel\'s rooms', err);
+            } else {
+                Reservation.remove({ _id: { $in: rooms.reservations } }, function (err) {
+                    err ? commons.sendError(req, res, 'Error in removing rooms', err) : '';
+                });
+
+                res.json({id: room._id});
+            }
+        });
+});
+
+module.exports = router;
